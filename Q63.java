d5b1145381f97a99619e3d4ed8cb42215206a0bd
ashish-and-vicky <62541813+ashish-and-vicky@users.noreply.github.com>
2020-10-08 01:52:01 +0530

    Update Q63.java

--- Q63.java
+++ Q63.java
@@ -11,18 +11,18 @@ public class Q63 {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		File f = new File("D:\\Adinath\\ACDAC\\java\\prac\\file1.txt");
+		File f = new File("D:\\Ashish\\DAC\\Java\\file1.txt");
 		if(!f.exists()){
 			f.createNewFile();
 			
 		}
-		FileWriter f1 = new FileWriter("D:\\Adinath\\ACDAC\\java\\prac\\file1.txt");
+		FileWriter f1 = new FileWriter("D:\\Ashish\\DAC\\Java\\file1.txt");
 		String str = "how are you t and t are u there t ttt";
 		BufferedWriter b1 = new BufferedWriter(f1);
 		f1.write(str);
 		b1.flush();
 		b1.close();
-		FileReader r1 = new FileReader("D:\\Adinath\\ACDAC\\java\\prac\\file1.txt");
+		FileReader r1 = new FileReader("D:\\Ashish\\DAC\\Java\\file1.txt");
 		int ch ;
 		int count = 0;
 		while((ch = r1.read()) != -1) {
